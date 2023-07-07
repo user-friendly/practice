551ecfe1673e21f9724e7a06f57077670edb0f85
User Friendly <pivanov@user-friendly.dev>
2023-07-07 14:21:47 -0400

    HackerRank: 30 days, day 29

---
+++ hackerrank/daysofcode/day_29/day_29.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'bitwiseAnd' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER N
+ *  2. INTEGER K
+ */
+
+function bitwiseAnd(N, K) {
+	let max = 0
+	
+    for (let i = 2; i < N; i++) {
+		for (let k = i + 1; k <= N; k++) {
+			if ((i & k) < K && (i & k) > max) {
+				max = (i & k)
+			}
+		}
+	}
+	
+	return max
+}
+
+function main() {
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const ws = process.stdout;
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const count = parseInt(firstMultipleInput[0], 10);
+
+        const lim = parseInt(firstMultipleInput[1], 10);
+
+        const res = bitwiseAnd(count, lim);
+
+        ws.write(res + '\n');
+    }
+
+    ws.end();
+}
