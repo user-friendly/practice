54a1fd13510d412ffb8a9a50b0b59fd6a16986ee
User Friendly <pivanov@user-friendly.dev>
2023-08-25 14:09:44 -0400

    LeetCode: `find middle pivot` - brute force

--- leetcode/find-the-middle-index-in-array/solution.py
+++ leetcode/find-the-middle-index-in-array/solution.py
@@ -1,5 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-def solve(nums):
-    raise Exception("TODO: Implement.")
+def solve(nums: list[int]) -> int:
+    # Brute force.
+    for i in range(len(nums)):
+        left = sum(nums[0:max(0,i)])
+        right = sum(nums[i+1:])
+        if left == right:
+            return i
+    
     return -1
