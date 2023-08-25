2bd544164f62ab73956e6e6483cad99beb5566d8
User Friendly <pivanov@user-friendly.dev>
2023-08-25 16:31:16 -0400

    LeetCode: `find middle pivot` linear verbose
    Somehow, this solution performed worse, at both time and memory.
    It probably has somehting to do with Python's optimizations.

--- leetcode/find-the-middle-index-in-array/solution.py
+++ leetcode/find-the-middle-index-in-array/solution.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-def solve(nums: list[int]) -> int:
-    # Brute force.
+def bruteForce(nums: list[int]):
     for i in range(len(nums)):
         left = sum(nums[0:max(0,i)])
         right = sum(nums[i+1:])
@@ -9,3 +8,27 @@ def solve(nums: list[int]) -> int:
             return i
     
     return -1
+
+def linearVerbose(nums: list[int]):
+    if len(nums) == 1:
+        return 0
+    
+    left = [0] * len(nums)
+    right = [0] * len(nums)
+    
+    left[1] = nums[1]
+
+    for i in range(1, len(nums)):
+        left[i] = left[i-1] + nums[i-1]
+    
+    for i in reversed(range(0, len(nums)-1)):    
+        right[i] = nums[i+1] + right[i+1]
+    
+    for i in range(len(nums)):
+        if left[i] == right[i]:
+            return i
+    
+    return -1
+
+def solve(nums: list[int]) -> int:
+    return linearVerbose(nums)
