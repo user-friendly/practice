da59256619e5b90b375930da35ab3a44b54926af
User Friendly <pivanov@user-friendly.dev>
2025-10-15 15:59:48 -0400

    LeetCode: Add printer proxies.

---
+++ leetcode/output-writer.js
@@ -0,0 +1,24 @@
+
+import { EOL } from "node:os"
+
+/**
+ * Proxy for stdout.write.
+ * 
+ * @param {any} chunk
+ * @returns
+ */
+function print(chunk, encoding = null, callback = null) {
+    return process.stdout.write(chunk, encoding, callback)
+}
+
+/**
+ * Proxy for stdout.write.
+ * 
+ * @param {string} chunk
+ * @returns
+ */
+function println(chunk, encoding = null, callback = null) {
+    return process.stdout.write(chunk + EOL, encoding = null, callback = null)
+}
+
+export { print, println }
