89cd10076e1668b69f529a86a6ce52895b4f32ca
User Friendly <pivanov@user-friendly.dev>
2025-10-16 12:22:26 -0400

    LeetCode: Update solution tpl.

--- leetcode/tpl/solution.js
+++ leetcode/tpl/solution.js
@@ -1,6 +1,7 @@
 import { inputReader } from '#lib/input-reader'
+import { print, println } from '#lib/output-writer'
 inputReader(solution, true)
 
 function solution(lines) {
-	lines.forEach((v) => console.log(v))
+	lines.forEach((v) => println(v))
 }
