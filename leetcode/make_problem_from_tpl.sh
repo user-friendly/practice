d5af96e3b8427f2bbc063e95bbe97c2bcce362ee
User Friendly <pivanov@user-friendly.dev>
2025-10-17 21:59:06 -0400

    LeetCode: Adjust make scripts.

--- leetcode/make_problem_from_tpl.sh
+++ leetcode/make_problem_from_tpl.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-if [ -d $1 ]; then
+SOL_DIR="solutions"
+SOL_TARGET="$SOL_DIR/$1"
+
+if [ -d $SOL_TARGET ]; then
 	echo "folder $1 already exists"
 	exit 1
 fi
 
-cp -r tpl $1
+cp -r tpl $SOL_TARGET
