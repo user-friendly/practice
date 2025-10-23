b6afe30426615d3f554792b855814e6a5b43cd4e
User Friendly <pivanov@user-friendly.dev>
2025-10-22 22:41:16 -0400

    LeetCode: Source problem #110.
    Needs more work.

--- leetcode/solutions/110/solution.js
+++ leetcode/solutions/110/solution.js
@@ -1,6 +1,26 @@
 import { inputReaderHelper } from '#lib/input-reader'
 import { TreeNode, TreeFromHeap, TreeWalkBreathFirst } from '#lib/binary-tree'
 
+var getDepth = function (node, depth = 0) {
+    depth++
+
+    const
+        a = node.left ? getDepth(node.left, depth) : depth,
+        b = node.right ? getDepth(node.right, depth) : depth
+
+    return a > b ? a : b
+}
+
+var getMinDepth = function (node, depth = 0) {
+    depth++
+
+    const
+        a = node.left ? getDepth(node.left, depth) : depth,
+        b = node.right ? getDepth(node.right, depth) : depth
+
+    return a > b ? b : a
+}
+
 /**
  * @param {TreeNode} root
  * @return {boolean}
@@ -8,7 +28,14 @@ import { TreeNode, TreeFromHeap, TreeWalkBreathFirst } from '#lib/binary-tree'
 var isBalanced = function (root) {
     // TreeWalkBreathFirst(root, (n, d) => console.log(`Node val: ${n.val}, parent: ${n.parent?.val}, depth: ${d}.`))
 
-    return false;
+    if (!root) {
+        return true
+    }
+
+    const max = getDepth(root)
+    const min = getMinDepth(root)
+
+    return max - min > 1 ? false : true;
 }
 
 inputReaderHelper((list) => isBalanced(TreeFromHeap(list)))
