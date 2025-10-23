502fd8f28e29d793f667bc1c597da1e143196e82
User Friendly <pivanov@user-friendly.dev>
2025-10-22 23:01:00 -0400

    LeetCode: Fix problem #110.
    Accepted solution.

--- leetcode/solutions/110/solution.js
+++ leetcode/solutions/110/solution.js
@@ -1,41 +1,31 @@
 import { inputReaderHelper } from '#lib/input-reader'
 import { TreeNode, TreeFromHeap, TreeWalkBreathFirst } from '#lib/binary-tree'
 
-var getDepth = function (node, depth = 0) {
-    depth++
-
-    const
-        a = node.left ? getDepth(node.left, depth) : depth,
-        b = node.right ? getDepth(node.right, depth) : depth
-
-    return a > b ? a : b
-}
-
-var getMinDepth = function (node, depth = 0) {
-    depth++
-
-    const
-        a = node.left ? getDepth(node.left, depth) : depth,
-        b = node.right ? getDepth(node.right, depth) : depth
-
-    return a > b ? b : a
-}
-
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
 var isBalanced = function (root) {
-    // TreeWalkBreathFirst(root, (n, d) => console.log(`Node val: ${n.val}, parent: ${n.parent?.val}, depth: ${d}.`))
-
     if (!root) {
         return true
     }
 
-    const max = getDepth(root)
-    const min = getMinDepth(root)
+    const checkBalance = (node) => {
+        if (!node) {
+            return 0
+        }
 
-    return max - min > 1 ? false : true;
+        const a = checkBalance(node.left),
+            b = checkBalance(node.right)
+
+        if (a === false || b === false || Math.abs(a - b) > 1) {
+            return false
+        }
+
+        return 1 + (a > b ? a : b)
+    }
+
+    return checkBalance(root) === false ? false : true
 }
 
 inputReaderHelper((list) => isBalanced(TreeFromHeap(list)))
