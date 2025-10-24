697e462dd25b39a626192e2f4f97d4f3e634d221
User Friendly <pivanov@user-friendly.dev>
2025-10-23 22:20:57 -0400

    LeetCode: Source problem #112.
    No idea if it works.

--- leetcode/solutions/112/solution.js
+++ leetcode/solutions/112/solution.js
@@ -7,14 +7,36 @@ import { TreeNode, TreeFromHeap } from '#lib/binary-tree'
  * @return {boolean}
  */
 var hasPathSum = function (root, targetSum) {
+    // No verts? No good.
+    if (!root) {
+        return false
+    }
+    // No children?
+    if (!(root.left || root.right)) {
+        // Easy-peasy.
+        return root.val === targetSum
+    }
 
     // Recursive solution:
     // Walking forward (id est depth first) accumulate and at the leaf determine if the sum is the target.
     // Return true if there is at least one match.
 
-    // TODO Implement.
+    const walker = (v, s = 0) => {
+        s += v.val
 
-    return false
+        if (!(v.left || v.right) && s === targetSum) {
+            return true
+        } else {
+            return false
+        }
+
+        const a = v.left ? walker(v.left, s) : null,
+            b = v.right ? walker(v.right, s) : null
+
+        return a || b
+    }
+
+    return walker(root)
 }
 
 inputReaderHelper((list) => hasPathSum(TreeFromHeap(list)))
