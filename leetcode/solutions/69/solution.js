7f93d87c6f28918b8a43ecf8a461acab685f090f
User Friendly <pivanov@user-friendly.dev>
2025-11-30 12:59:50 -0500

    LeetCode: Brute force problem #69.

--- leetcode/solutions/69/solution.js
+++ leetcode/solutions/69/solution.js
@@ -7,7 +7,18 @@ import { inputReaderHelper } from '#lib/input-reader'
  */
 var mySqrt = function (x) {
     // Naive way, will not be accepted at an interview.
-    return Math.floor(Math.sqrt(x))
+    // return Math.floor(Math.sqrt(x))
+
+    if (x === 0 || x === 1) {
+        return x
+    }
+
+    // Brute force.
+    let r = 2
+    while (r * r < x) {
+        r += 1
+    }
+    return (r * r === x) ? r : r - 1
 }
 
 inputReaderHelper(mySqrt)
