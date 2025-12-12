e27f704b3fe66e5d6bd5a5695d5ac0ce769f6cc1
User Friendly <pivanov@user-friendly.dev>
2025-12-12 12:47:01 -0500

    LeetCode: Convert #70 from JS to TS.

---
+++ leetcode/solutions/70/solution.ts
@@ -0,0 +1,17 @@
+import { inputReaderHelper } from '#lib/input-reader'
+
+/**
+ * 
+ * 2: 1 1
+ * 3: 1 1 1
+ * 4: 1 1 1 1
+ * 5: 1 1 1 1 1
+ * 6: 1 1 1 1 1 1
+ * 7: 1 1 1 1 1 1 1
+ * 
+ */
+function climbStairs(n: number): number {
+    return -1
+}
+
+inputReaderHelper(climbStairs)
