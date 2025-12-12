bbaf1e18d01f5dacbb6d6535a2b79616447689a7
User Friendly <pivanov@user-friendly.dev>
2025-12-12 12:46:23 -0500

    LeetCode: Enable TypeScript scripts.

--- leetcode/test_solution.sh
+++ leetcode/test_solution.sh
@@ -10,8 +10,11 @@ fi
 
 NAME_DIR="$1"
 EXT="js"
+EXT_TS="ts"
 SOL_DIR="solutions"
-SOL_FILE="$SOL_DIR/$NAME_DIR/solution.$EXT"
+SOL_FILE=""
+SOL_FILE_JS="$SOL_DIR/$NAME_DIR/solution.$EXT"
+SOL_FILE_TS="$SOL_DIR/$NAME_DIR/solution.$EXT_TS"
 INPUT_FILE="$SOL_DIR/$NAME_DIR/data.input"
 # Desired output, i.e. solution answers.
 OUTPUT_FILE="$SOL_DIR/$NAME_DIR/data.output"
@@ -22,6 +25,14 @@ if [ ! "x$2" = "x" ]; then
 	if [ ! "x$3" = "x" ]; then
 		EXT="$3"
 	fi
+fi
+
+# We're dealing with a TypeScript solution.
+if [ -f $SOL_FILE_TS ]; then
+	INTERP="$INTERP --import tsx"
+	SOL_FILE="$SOL_FILE_TS"
+else
+	SOL_FILE="$SOL_FILE_JS"
 fi
 
 cat $INPUT_FILE | $INTERP $SOL_FILE | $DIFFER $OUTPUT_FILE -
