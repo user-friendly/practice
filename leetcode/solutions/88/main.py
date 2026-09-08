c9f928afd212ac30aa9f7de2a6faae6bea2a12f3
User Friendly <pivanov@user-friendly.dev>
2026-09-08 10:50:48 -0400

    LeetCode: Source problem #88.

--- leetcode/solutions/88/main.py
+++ leetcode/solutions/88/main.py
@@ -9,7 +9,7 @@ import testcases
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         # Deal with the two edge cases. Should save time.
-        
+
         if n <= 0:
             for k, v in enumerate(nums1):
                 nums1[k] = v if k < m else 0
@@ -19,18 +19,56 @@ class Solution:
                 nums1[k] = nums2[k] if k < n else 0
             return None
 
+        MIN = int(-1.1e9)
         j = m - 1
         k = n - 1
-        for i in reversed(range(m + n)):
-            if nums1[j] > nums2[k]:
+        i = m + n - 1
+
+        # # Less then ideal solution.
+        # tmp_list = [MIN] * (m + n)
+        # while i >= 0:
+        #     if j >= 0 and k >= 0:
+        #         if nums1[j] > nums2[k]:
+        #             tmp_list[i] = nums1[j]
+        #             j -= 1
+        #         elif nums2[k] > nums1[j]:
+        #             tmp_list[i] = nums2[k]
+        #             k -= 1
+        #         elif nums2[k] == nums1[j]:
+        #             tmp_list[i] = nums2[k]
+        #             k -= 1
+        #     elif j >= 0:
+        #         tmp_list[i] = nums1[j]
+        #         j -= 1
+        #     elif k >= 0:
+        #         tmp_list[i] = nums2[k]
+        #         k -= 1
+            
+        #     i -= 1
+        #     pass
+
+        while i >= 0:
+            if j >= 0 and k >= 0:
+                if nums1[j] > nums2[k]:
+                    nums1[i] = nums1[j]
+                    nums1[j] = MIN
+                    j -= 1
+                elif nums2[k] > nums1[j]:
+                    nums1[i] = nums2[k]
+                    k -= 1
+                elif nums2[k] == nums1[j]:
+                    nums1[i] = nums2[k]
+                    k -= 1
+            elif j >= 0:
                 nums1[i] = nums1[j]
-                nums1[i] = 0
                 j -= 1
-            elif nums2[k] > nums1[j]:
+                pass
+            elif k >= 0:
                 nums1[i] = nums2[k]
                 k -= 1
-            
-            
+                pass
+
+            i -= 1
             pass
 
         return None
